95b846a7d4c4cbc3b1f14cf7ca99a7cee027a3a9
Maxim <lmaokeklol@gmail.com>
2023-02-18 16:21:50 +0300

    add gunicorn setup to init.sh

--- init.sh
+++ init.sh
@@ -2,3 +2,6 @@ sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo rm -rf /etc/nginx/sites-enabled/default
 sudo nginx -t
 sudo /etc/init.d/nginx restart
+
+sudo ln -sf /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
+sudo /etc/init.d/gunicorn restart

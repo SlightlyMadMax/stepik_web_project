d2728c93e9166f38a54fa3bd9435c383082b6658
Maxim <lmaokeklol@gmail.com>
2023-02-19 15:08:24 +0300

    fix typo

--- ask/ask/settings.py
+++ ask/ask/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'ask.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'stepic_web',
+        'NAME': 'stepik_web',
         'USER': 'box',
     }
 }

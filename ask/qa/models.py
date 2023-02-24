830041e5831e13a15eb36d7d8be4237ffa834237
Maxim <lmaokeklol@gmail.com>
2023-02-24 23:22:36 +0300

    minor fix

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -28,8 +28,8 @@ class Question(models.Model):
 
 
 class Answer(models.Model):
-    text = models.TextField()
-    added_at = models.DateTimeField(auto_now_add=True, null=True)
+    text = models.TextField(default="")
+    added_at = models.DateTimeField(auto_now_add=True)
     question = models.OneToOneField(to=Question, on_delete=models.CASCADE, related_name='answer_set')
     author = models.ForeignKey(to=User, on_delete=models.CASCADE, related_name='answers')
 

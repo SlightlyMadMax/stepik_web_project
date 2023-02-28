661325b82bd8f404b3ad9a1851c623724aa5da2f
Maxim <lmaokeklol@gmail.com>
2023-02-28 19:12:19 +0300

    remove user from forms

--- ask/qa/forms.py
+++ ask/qa/forms.py
@@ -6,15 +6,13 @@ class AskForm(forms.Form):
     title = forms.CharField(max_length=255)
     text = forms.CharField(widget=forms.Textarea)
 
-    def __init__(self, user=None, *args, **kwargs):
-        self._user = user
+    def __init__(self, *args, **kwargs):
         super(AskForm, self).__init__(*args, **kwargs)
 
     def clean(self):
-        return self.cleaned_data
+        pass
 
     def save(self):
-        self.cleaned_data['author'] = self._user
         return Question.objects.create(**self.cleaned_data)
 
 
@@ -22,13 +20,11 @@ class AnswerForm(forms.Form):
     text = forms.CharField(widget=forms.Textarea)
     question = forms.ModelChoiceField(queryset=Question.objects.all(), widget=forms.HiddenInput)
 
-    def __init__(self, user=None, *args, **kwargs):
-        self._user = user
+    def __init__(self, *args, **kwargs):
         super(AnswerForm, self).__init__(*args, **kwargs)
 
     def clean(self):
-        return self.cleaned_data
+        pass
 
     def save(self):
-        self.cleaned_data['author'] = self._user
         return Answer.objects.create(**self.cleaned_data)

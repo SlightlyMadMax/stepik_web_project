b19f6981f75ef33404906ded1c43d3e02564fcaf
Maxim <lmaokeklol@gmail.com>
2023-03-08 20:16:16 +0300

    fix login form

--- ask/qa/views.py
+++ ask/qa/views.py
@@ -120,7 +120,7 @@ def login_view(request):
             user = authenticate(username=username, password=password)
             if user is not None:
                 login(request, user)
-                return HttpResponseRedirect(reverse('new_questions'))
+            return HttpResponseRedirect(reverse('new_questions'))
         error = u'Неверный логин / пароль'
     else:
         form = LoginForm()
